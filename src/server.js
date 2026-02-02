044f375871862ffb4e953aca24af6e746bcc9c89
RANDRIANARISOA Ny Avohasina Mampandry <120253454+N1A1voIt@users.noreply.github.com>
2026-02-02 23:59:20 +0300

    Init express env

---
+++ src/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const app = require('./app');
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.listen(PORT, () => console.log('Server running on port', PORT));
